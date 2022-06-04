4f9b242ce841c2a7fd643f9b09c03c5b9ac5897a
Rebbit13 <errb13@gmail.com>
2022-06-04 16:27:38 +0400

    add human handler

--- internal/handlers/human.go
+++ internal/handlers/human.go
@@ -1,8 +1,93 @@
 package handlers
 
-import "go.uber.org/zap"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+	"strconv"
+)
 
 type Human struct {
 	service HumanService
 	logger  *zap.Logger
+}
+
+func (h *Human) Create(context *gin.Context) {
+	human := &HumanToUpdateAndCreate{}
+	err := context.BindJSON(human)
+	if err != nil {
+		return
+	}
+	humanCreated, err := h.service.Create(human.convertToGORMModel())
+	if err != nil {
+		h.logger.Error(err.Error(), zap.String("status_code", "500"))
+		context.Status(500)
+		return
+	}
+	context.JSON(201, humanCreated)
+}
+
+func (h *Human) Get(context *gin.Context) {
+	humanId, err := strconv.ParseUint(context.Param("id"), 10, 64)
+	if err != nil {
+		h.logger.Error(err.Error(), zap.String("status_code", "400"))
+		context.JSON(400, json_message("Can not parse id. Must be unsigned integer."))
+		return
+	}
+	human, err := h.service.Get(uint(humanId))
+	if err != nil {
+		h.logger.Error(err.Error(), zap.String("status_code", "404"))
+		context.JSON(404, json_message(fmt.Sprintf("There is no human with id %d", humanId)))
+		return
+	}
+	context.JSON(200, human)
+}
+
+func (h *Human) GetAll(context *gin.Context) {
+	humans, err := h.service.GetAll()
+	if err != nil {
+		h.logger.Error(err.Error(), zap.String("status_code", "500"))
+		context.Status(500)
+		return
+	}
+	context.JSON(200, humans)
+}
+
+func (h *Human) Update(context *gin.Context) {
+	humanId, err := strconv.ParseUint(context.Param("id"), 10, 64)
+	if err != nil {
+		h.logger.Error(err.Error(), zap.String("status_code", "400"))
+		context.JSON(400, json_message("Can not parse id. Must be unsigned integer."))
+		return
+	}
+	human := &HumanToUpdateAndCreate{}
+	err = context.BindJSON(human)
+	if err != nil {
+		return
+	}
+	humanToUpdate := human.convertToGORMModel()
+	humanToUpdate.ID = uint(humanId)
+	humanUpdated, err := h.service.Update(humanToUpdate)
+	if err != nil {
+		h.logger.Error(err.Error(), zap.String("status_code", "500"))
+		context.Status(500)
+		return
+	}
+	context.JSON(200, humanUpdated)
+}
+
+func (h *Human) Delete(context *gin.Context) {
+	humanId, err := strconv.ParseUint(context.Param("id"), 10, 64)
+	if err != nil {
+		h.logger.Error(err.Error(), zap.String("status_code", "400"))
+		context.JSON(400, json_message("Can not parse id. Must be unsigned integer."))
+		return
+	}
+	err = h.service.Delete(uint(humanId))
+	if err != nil {
+		h.logger.Error(err.Error(), zap.String("status_code", "404"))
+		context.JSON(404, json_message(fmt.Sprintf("There is no human with id %d", humanId)))
+		return
+	}
+	context.Status(204)
 }
--- internal/handlers/rest_models.go
+++ internal/handlers/rest_models.go
@@ -1,11 +1,24 @@
 package handlers
 
-import "groups/internal/models"
+import (
+	"groups/internal/models"
+	"time"
+)
 
 type GroupToUpdateAndCreate struct {
-	Name string
+	Name string `json:"name" binding:"required"`
 }
 
 func (g *GroupToUpdateAndCreate) convertToGORMModel() *models.Group {
 	return &models.Group{Name: g.Name}
+}
+
+type HumanToUpdateAndCreate struct {
+	Name      string    `json:"name" binding:"required"`
+	Surname   string    `json:"surname" binding:"required"`
+	Birthdate time.Time `json:"birthdate" binding:"required"`
+}
+
+func (h *HumanToUpdateAndCreate) convertToGORMModel() *models.Human {
+	return &models.Human{Name: h.Name, Surname: h.Surname, Birthdate: h.Birthdate}
 }
