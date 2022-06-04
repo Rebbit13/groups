aa727e7ab949438fc149014ed005eaeec0b9c5a1
Rebbit13 <errb13@gmail.com>
2022-06-04 11:08:04 +0400

    add models

--- internal/models/human.go
+++ internal/models/human.go
@@ -1,1 +1,14 @@
 package models
+
+import (
+	"gorm.io/gorm"
+	"time"
+)
+
+type Human struct {
+	gorm.Model
+	Name      string    `json:"name" binding:"required"`
+	Surname   string    `json:"surname" binding:"required"`
+	Birthdate time.Time `json:"birthdate" binding:"required"`
+	Groups    []Group   `gorm:"many2many:humans_groups;" json:"groups" binding:"required"`
+}
